4e48c946aea2fb8c49cc2b9aeaae9544fe2963a4
timofey.homich <timofey.homich@gmail.com>
2025-09-28 19:33:03 +0300

    change styles

--- DCT_WPF/MainWindow.xaml.cs
+++ DCT_WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using DCT_WPF.ViewModel;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace DCT_WPF
 {
@@ -10,6 +11,40 @@ namespace DCT_WPF
             InitializeComponent();
 
             DataContext = new MainViewModel();
+        }
+
+        private void ChangeLanguage_BtnClick(object sender, RoutedEventArgs e)
+        {
+            SetLanguage(((Button)sender).Tag.ToString());
+        }
+
+        private void SetLanguage(string language)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(language);
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(language);
+
+            Application.Current.Resources.MergedDictionaries.Clear();
+            ResourceDictionary resdict = new ResourceDictionary()
+            {
+                Source = new Uri($"/Localization/Dicitionary-{language}.xaml", UriKind.Relative)
+            };
+
+            Application.Current.Resources.MergedDictionaries.Add(resdict);
+
+            EngButton.IsEnabled = true;
+            UaButton.IsEnabled = true;
+
+            switch (language)
+            {
+                case "en":
+                    EngButton.IsEnabled = false;
+                    break;
+                case "ua":
+                    UaButton.IsEnabled = false;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
